a4e62a8d2515141c8822c2ab887c9f98e33476ca
gabriel <131698112+leirahf@users.noreply.github.com>
2024-11-19 15:08:28 -0300

    Update CORS index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Importa o Express
 const express = require('express');
+const cors = require('cors'); // Importa o pacote CORS
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Ativa o CORS para todas as rotas
+app.use(cors());
 
 // Middleware para interpretar JSON
 app.use(express.json());
