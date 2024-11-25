b5797070ca35b9983ad064995ee4b15c0f71426f
gabriel <131698112+leirahf@users.noreply.github.com>
2024-11-25 20:36:56 -0300

    Update index.js
    Mudança no valor da porta de protocolo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors'); // Importa o pacote CORS
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Middleware para interpretar JSON
 app.use(express.json());
