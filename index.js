f6010d60f9e76107d8f5f059009324c3db82b1dd
DELL <gabrielsouza715977@gmail.com>
2024-12-02 12:24:26 -0300

    Atualizando index.js e package.json e adicionando pasta public

--- index.js
+++ index.js
@@ -1,50 +1,15 @@
-// Importa o Express
-const express = require('express');
-const cors = require('cors'); // Importa o pacote CORS
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
-// Middleware para interpretar JSON
-app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
 
-// Configuração do CORS para permitir requisições de outras origens
-app.use(cors()); // Permite todas as origens
-
-// Variável para armazenar a mensagem
-let mensagemArmazenada = "";
-
-// Forçar HTTP em vez de HTTPS, verificando o cabeçalho X-Forwarded-Proto
-app.use((req, res, next) => {
-  if (req.protocol === 'https') {
-    // Se o tráfego vem de HTTPS, redireciona para HTTP
-    return res.redirect('http://' + req.headers.host + req.url);
-  }
-  next();
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/mapa_fetch.html");
 });
 
-// Endpoint para armazenar a mensagem (POST)
-app.post('/api/armazenar_mensagem', (req, res) => {
-  mensagemArmazenada = req.body.conteudo;
-  res.json({ status: "Mensagem armazenada com sucesso" });
-});
-
-// Endpoint alternativo para armazenar a mensagem via GET
-app.get('/api/armazenar_mensagem_get', (req, res) => {
-  const mensagem = req.query.mensagem; // Recupera o parâmetro "mensagem" da query string
-  if (mensagem) {
-    mensagemArmazenada = mensagem;
-    res.json({ status: "Mensagem armazenada com sucesso (via GET)" });
-  } else {
-    res.status(400).json({ status: "Erro: Mensagem não fornecida!" });
-  }
-});
-
-// Endpoint para recuperar a mensagem (GET)
-app.get('/api/obter_mensagem', (req, res) => {
-  res.json({ conteudo: mensagemArmazenada });
-});
-
-// Inicia o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
